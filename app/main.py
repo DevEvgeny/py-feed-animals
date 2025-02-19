bdd08502199af6314ebd3b17a164d4bf93248ace
Polevoy Yevhen <polevoye90@gmail.ua>
2025-02-19 13:34:04 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ class Animal:
         self.is_hungry = is_hungry
 
     def print_name(self) -> None:
-        print("Hello, I'm {self.name}")
+        print(f"Hello, I'm {self.name}")
 
     def feed(self) -> int:
         if self.is_hungry:
